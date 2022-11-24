23922f56e9ba13e580aeee9553006253912a7742
David Espino <davidespinog@gmail.com>
2022-11-24 11:54:05 -0600

    styled component props fixed

--- src/components/MainTitle.js
+++ src/components/MainTitle.js
@@ -2,22 +2,18 @@ import React from "react";
 import "./MainTitle.css";
 import styled from "styled-components";
 import "./MainTitle.css";
-const getStyledDiv = (imgUrl) => {
-    const StyledDiv = styled.div`
-        background:linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${imgUrl});
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        height: 31rem;
-        border-radius: 1rem;
-        padding-right:2rem;
-    `
-    return { StyledDiv }
-} 
+const StyledDiv = styled.div`
+    background:linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${props => props.imgUrl});
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 31rem;
+    border-radius: 1rem;
+    padding-right:2rem;
+`
 function MainTitle({title, img}){
-    const {StyledDiv} = getStyledDiv(img)
     return (
-        <StyledDiv className="title-container">
+        <StyledDiv imgUrl={img} className="title-container">
             <h1 id="title-container__title">{title}</h1>
         </StyledDiv>
     )
